99b59b5a786ff88640f38af7f740aba900868b02
Chirag-S-kotian <ckotian117@gmail.com>
2024-05-01 18:07:10 +0530

    Updated jarvis.py with new features and bug fixes
    - Changed the way the voice is initialized
    - Added functions for time and date
    - Updated wishMe function with improved greetings
    - Added exception handling in

--- jarvis.py
+++ jarvis.py
@@ -1,92 +1,172 @@
-import speech_recognition as sr
 import pyttsx3
+import pywin32_system32
 import datetime
+import speech_recognition as sr
 import wikipedia
-import webbrowser
+import webbrowser as wb
 import os
+import random
+import pyautogui
+from PIL import Image
 
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+engine = pyttsx3.init()
+
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-def wishMe():
-    hour = int(datetime.datetime.now().hour)
-    if hour>=0 and hour<12:
-        speak("Good Morning!")
-    elif hour>=12 and hour<18:
-        speak("Good Afternoon!")
-    else:
-        speak("Good Evening!")
-    speak("I am Jarvis Sir. tell me your command ")
+def time():
+    Time = datetime.datetime.now().strftime("%I:%M:%S")
+    speak("the current time is")
+    speak(Time)
+    print("The current time is ", Time)
+
+def date():
+    day = int(datetime.datetime.now().day)
+    month = int(datetime.datetime.now().month)
+    year = int(datetime.datetime.now().year)
+    speak("the current date is")
+    speak(day)
+    speak(month)
+    speak(year)
+    print("The current date is " + str(day) + "/" + str(month) + "/" + str(year))
+
+def wishme():
+    print("Welcome back sir!!")
+    speak("Welcome back sir!!")
     
-def takeCommand():
+    hour = datetime.datetime.now().hour
+    if hour >= 4 and hour < 12:
+        speak("Good Morning Sir!!")
+        print("Good Morning Sir!!")
+    elif hour >= 12 and hour < 16:
+        speak("Good Afternoon Sir!!")
+        print("Good Afternoon Sir!!")
+    elif hour >= 16 and hour < 24:
+        speak("Good Evening Sir!!")
+        print("Good Evening Sir!!")
+    else:
+        speak("Good Night Sir, See You Tommorrow")
+
+    speak("Jarvis at your service sir, please tell me how may I help you.")
+    print("Jarvis at your service sir, please tell me how may I help you.")
+
+def screenshot():
+    img = pyautogui.screenshot()
+    img_path = os.path.expanduser("~\\Pictures\\ss.png")
+    img.save(img_path)
+
+
+def takecommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
         r.pause_threshold = 1
         audio = r.listen(source)
+
     try:
         print("Recognizing...")
-        query = r.recognize_google(audio, language='en-in')
-        print(f"User said: {query}\n")
+        query = r.recognize_google(audio, language="en-in")
+        print(query)
+
     except Exception as e:
-        # print(e)
-        print("Say that again please...")
-        return "None"
+        print(e)
+        speak("Please say that again")
+        return "Try Again"
+
     return query
 
-if __name__ == '__main__':
-    wishMe()
+if __name__ == "__main__":
+    wishme()
     while True:
-    # if 1:
-        query = takeCommand().lower()
-    
-        #logic for executing tasks
-        if 'wikipedia' in query:
-            speak('Searching Wikipedia...')
-            query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=2)
-            speak("According to Wikipedia")
-            print(results)
-            speak(results)
-        elif 'open youtube' in query:
-            webbrowser.open("youtube.com")
-        elif 'open google' in query:
-            webbrowser.open("google.com")
-        elif 'open stackoverflow' in query:
-            webbrowser.open("stackoverflow.com")
-        elif 'play music' in query:
-            music_dir = 'C:\\Users\\Sameeksha S Kotian\\Music'
-            songs = os.listdir(music_dir)
-            print(songs)
-            os.startfile(os.path.join(music_dir, songs[0]))
-        elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"Sir, the time is {strTime}")
-        elif 'open vscode' in query:
-            codePath = "C:\\Users\\Sameeksha S Kotian\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
-            os.startfile(codePath)
-        elif 'open notepad' in query:
-            codePath = "C:\\Windows\\System32\\notepad.exe"
-            os.startfile(codePath)
-        elif 'open chrome' in query:
-            codePath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-            os.startfile(codePath)
-        elif 'open cmd' in query:
-            codePath = "C:\\Windows\\System32\\cmd.exe"
-            os.startfile(codePath)
-        elif 'open videos' in query:
-            codePath = "C:\\Users\\Sameeksha S Kotian\\Videos"
-            os.startfile(codePath)
-        elif 'open images' in query:
-            codePath = "C:\\Users\\Sameeksha S Kotian\\Pictures"
-            os.startfile(codePath)
-            speak("Here are some of the images")
-        elif 'bye' in query or 'quit' in query:
-            speak("Goodbye Sir. Have a graet day!")
-            break
+        query = takecommand().lower()
+        if "time" in query:
+            time()
+
+        elif "date" in query:
+            date()
+
+        elif "who are you" in query:
+            speak("I'm JARVIS created by Mr. Chirag and I'm a desktop voice assistant.")
+            print("I'm JARVIS created by Mr. Chirag and I'm a desktop voice assistant.")
+
+        elif "how are you" in query:
+            speak("I'm fine sir, What about you?")
+            print("I'm fine sir, What about you?")
+
+        elif "fine" in query:
+            speak("Glad to hear that sir!!")
+            print("Glad to hear that sir!!")
+
+        elif "good" in query:
+            speak("Glad to hear that sir!!")
+            print("Glad to hear that sir!!")
+
+        elif "wikipedia" in query:
+            try:
+                speak("Ok wait sir, I'm searching...")
+                query = query.replace("wikipedia","")
+                result = wikipedia.summary(query, sentences=2)
+                print(result)
+                speak(result)
+            except:
+                speak("Can't find this page sir, please ask something else")
         
-    
+        elif "open youtube" in query:
+            wb.open("youtube.com") 
+
+        elif "open google" in query:
+            wb.open("google.com") 
+   
+        elif "open stack overflow" in query:
+            wb.open("stackoverflow.com")
+
+        elif "play music" in query:
+            song_dir = os.path.expanduser("~\\Music")
+            songs = os.listdir(song_dir)
+            print(songs)
+            x = len(songs)
+            y = random.randint(0,x)
+            os.startfile(os.path.join(song_dir, songs[y]))
+
+        elif "open chrome" in query:
+            chromePath = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+            os.startfile(chromePath)
+
+        elif "search on chrome" in query:
+            try:
+                speak("What should I search?")
+                print("What should I search?")
+                chromePath = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+                search = takecommand()
+                wb.get(chromePath).open_new_tab(search)
+                print(search)
+
+            except Exception as e:
+                speak("Can't open now, please try again later.")
+                print("Can't open now, please try again later.")
+            
+        
+        elif "remember that" in query:
+            speak("What should I remember")
+            data = takecommand()
+            speak("You said me to remember that" + data)
+            print("You said me to remember that " + str(data))
+            remember = open("data.txt", "w")
+            remember.write(data)
+            remember.close()
+
+        elif "do you remember anything" in query:
+            remember = open("data.txt", "r")
+            speak("You told me to remember that" + remember.read())
+            print("You told me to remember that " + str(remember))
+
+        elif "screenshot" in query:
+            screenshot()
+            speak("I've taken screenshot, please check it")
+
+
+        elif "offline" in query:
+            quit()
+
+
